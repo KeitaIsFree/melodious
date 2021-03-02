c5d6cfe18361bd83cd14f0f3bea9712bd7d2563b
KeitaIsFree <maxdxd10108102@gmail.com>
2021-03-02 19:57:34 +0900

    [Add] Migrate code from SynthUsingMidi tutorial file

--- Melodious/Source/MainComponent.cpp
+++ Melodious/Source/MainComponent.cpp
@@ -1,80 +1,153 @@
 #include "MainComponent.h"
 
+
 //==============================================================================
 MainComponent::MainComponent()
+  : synthAudioSource (keyboardState),
+	keyboardComponent (keyboardState, juce::MidiKeyboardComponent::horizontalKeyboard),
+	audioSetupComp (deviceManager,
+					0,     // minimum input channels
+					256,   // maximum input channels
+					0,     // minimum output channels
+					256,   // maximum output channels
+					false, // ability to select midi inputs
+					false, // ability to select midi output device
+					false, // treat channels as stereo pairs
+					false) // hide advanced options
 {
-    // Make sure you set the size of the component after
-    // you add any child components.
-    setSize (800, 600);
 
-    // Some platforms require permissions to open input channels so request that here
-    if (juce::RuntimePermissions::isRequired (juce::RuntimePermissions::recordAudio)
-        && ! juce::RuntimePermissions::isGranted (juce::RuntimePermissions::recordAudio))
-    {
-        juce::RuntimePermissions::request (juce::RuntimePermissions::recordAudio,
-                                           [&] (bool granted) { setAudioChannels (granted ? 2 : 0, 2); });
-    }
-    else
-    {
-        // Specify the number of input and output channels that we want to open
-        setAudioChannels (2, 2);
-    }
+  addAndMakeVisible (keyboardComponent);
+
+  addAndMakeVisible (audioSetupComp);
+  // Make sure you set the size of the component after
+  // you add any child components.
+  setSize (800, 600);
+
+  // Some platforms require permissions to open input channels so request that here
+  // if (juce::RuntimePermissions::isRequired (juce::RuntimePermissions::recordAudio)
+  // 	  && ! juce::RuntimePermissions::isGranted (juce::RuntimePermissions::recordAudio))
+  //   {
+  // 	  juce::RuntimePermissions::request (juce::RuntimePermissions::recordAudio,
+  // 										 [&] (bool granted) { setAudioChannels (granted ? 2 : 0, 2); });
+  //   }
+  // else
+  //   {
+  // 	  // Specify the number of input and output channels that we want to open
+  // 	  setAudioChannels (2, 2);
+  //   }
+  
+  setAudioChannels (0, 2);
+
+  startTimer (400);
+  addAndMakeVisible (midiInputListLabel);
+	midiInputListLabel.setText ("MIDI Input:", juce::dontSendNotification);
+	midiInputListLabel.attachToComponent (&midiInputList, true);
+ 
+	auto midiInputs = juce::MidiInput::getAvailableDevices();
+	addAndMakeVisible (midiInputList);
+	midiInputList.setTextWhenNoChoicesAvailable ("No MIDI Inputs Enabled");
+ 
+	juce::StringArray midiInputNames;
+	for (auto input : midiInputs)
+	  midiInputNames.add (input.name);
+ 
+	midiInputList.addItemList (midiInputNames, 1);
+	midiInputList.onChange = [this] { setMidiInput (midiInputList.getSelectedItemIndex()); };
+ 
+	for (auto input : midiInputs)
+	  {
+		if (deviceManager.isMidiInputDeviceEnabled (input.identifier))
+		  {
+			setMidiInput (midiInputs.indexOf (input));
+			break;
+		  }
+	  }
+ 
+	if (midiInputList.getSelectedId() == 0)
+	  setMidiInput (0);
+  
 }
 
 MainComponent::~MainComponent()
 {
-    // This shuts down the audio device and clears the audio source.
-    shutdownAudio();
+  // This shuts down the audio device and clears the audio source.
+  shutdownAudio();
+}
+
+void MainComponent::setMidiInput (int index)
+{
+  auto list = juce::MidiInput::getAvailableDevices();
+ 
+  deviceManager.removeMidiInputDeviceCallback (list[lastInputIndex].identifier, synthAudioSource.getMidiCollector());
+ 
+  auto newInput = list[index];
+ 
+  if (! deviceManager.isMidiInputDeviceEnabled (newInput.identifier))
+	deviceManager.setMidiInputDeviceEnabled (newInput.identifier, true);
+ 
+  deviceManager.addMidiInputDeviceCallback (newInput.identifier, synthAudioSource.getMidiCollector());
+  midiInputList.setSelectedId (index + 1, juce::dontSendNotification);
+ 
+  lastInputIndex = index;
 }
 
 //==============================================================================
 void MainComponent::prepareToPlay (int samplesPerBlockExpected, double sampleRate)
 {
-    // This function will be called when the audio device is started, or when
-    // its settings (i.e. sample rate, block size, etc) are changed.
+  // This function will be called when the audio device is started, or when
+  // its settings (i.e. sample rate, block size, etc) are changed.
 
-    // You can use this function to initialise any resources you might need,
-    // but be careful - it will be called on the audio thread, not the GUI thread.
+  // You can use this function to initialise any resources you might need,
+  // but be careful - it will be called on the audio thread, not the GUI thread.
 
-    // For more details, see the help for AudioProcessor::prepareToPlay()
+  // For more details, see the help for AudioProcessor::prepareToPlay()
+  synthAudioSource.prepareToPlay (samplesPerBlockExpected, sampleRate);
 }
 
 void MainComponent::getNextAudioBlock (const juce::AudioSourceChannelInfo& bufferToFill)
 {
-    // Your audio-processing code goes here!
+  // Your audio-processing code goes here!
 
-    // For more details, see the help for AudioProcessor::getNextAudioBlock()
+  // For more details, see the help for AudioProcessor::getNextAudioBlock()
 
-    // Right now we are not producing any data, in which case we need to clear the buffer
-    // (to prevent the output of random noise)
-    bufferToFill.clearActiveBufferRegion();
+  // Right now we are not producing any data, in which case we need to clear the buffer
+  // (to prevent the output of random noise)
+  synthAudioSource.getNextAudioBlock (bufferToFill);
 }
 
 void MainComponent::releaseResources()
 {
-    // This will be called when the audio device stops, or when it is being
-    // restarted due to a setting change.
+  // This will be called when the audio device stops, or when it is being
+  // restarted due to a setting change.
 
-    // For more details, see the help for AudioProcessor::releaseResources()
+  // For more details, see the help for AudioProcessor::releaseResources()
+  synthAudioSource.releaseResources();
 }
 
 //==============================================================================
 void MainComponent::paint (juce::Graphics& g)
 {
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+  // (Our component is opaque, so we must completely fill the background with a solid colour)
+  g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
-    // You can add your drawing code here!
+  // You can add your drawing code here!
 }
 
 void MainComponent::timerCallback()
 {
   // Animation update here
+  keyboardComponent.grabKeyboardFocus();
+  stopTimer();
 }
 
 void MainComponent::resized()
 {
-    // This is called when the MainContentComponent is resized.
-    // If you add any child components, this is where you should
-    // update their positions.
+  // This is called when the MainContentComponent is resized.
+  // If you add any child components, this is where you should
+  // update their positions.auto rect = getLocalBounds();
+
+  auto rect = getLocalBounds();
+  audioSetupComp.setBounds (rect.removeFromLeft (proportionOfWidth (0.6f)));
+  midiInputList    .setBounds (200, 10, getWidth() - 210, 20);
+  keyboardComponent.setBounds (10, 10, getWidth() - 20, getHeight() - 20);
 }
--- Melodious/Source/MainComponent.h
+++ Melodious/Source/MainComponent.h
@@ -2,32 +2,266 @@
 
 #include <JuceHeader.h>
 
+struct SineWaveSound : public juce::SynthesiserSound
+{
+  SineWaveSound() {}
+
+  bool appliesToNote (int) override { return true; }
+  bool appliesToChannel (int) override { return true; }
+};
+
+struct SineWaveVoice : public juce::SynthesiserVoice
+{
+  SineWaveVoice (const juce::AudioSampleBuffer& wavetableToUse)
+	: wavetable (wavetableToUse),
+	  tableSize (wavetable.getNumSamples()) {}
+  bool canPlaySound (juce::SynthesiserSound* sound) override
+  {
+	return dynamic_cast<SineWaveSound*> (sound) != nullptr;
+  }
+
+  void startNote (int midiNoteNumber, float velocity,
+				  juce::SynthesiserSound*, int /*currentPitchWheelPosition*/) override
+  {
+	currentIndex = 0.0;
+	level = velocity * 0.15;
+	tailOff = 0.0;
+
+	auto cyclesPerSecond = juce::MidiMessage::getMidiNoteInHertz (midiNoteNumber);
+	tableDelta = (float) cyclesPerSecond / getSampleRate() * (float) tableSize;
+  }
+
+  void stopNote (float /*velocity*/, bool allowTailOff) override
+  {
+	if (allowTailOff)
+	  {
+		if (tailOff == 0.0)
+		  tailOff = 1.0;
+	  }
+	else
+	  {
+		clearCurrentNote();
+		tableDelta = 0.0;
+	  }		  
+  }
+
+  void pitchWheelMoved (int) override {}
+  void controllerMoved (int, int) override {}
+
+  void renderNextBlock (juce::AudioSampleBuffer& outputBuffer, int startSample, int numSamples) override
+  {
+	if (tableDelta != 0.0)
+	  {
+		while (--numSamples >= 0)
+		  {
+		    
+			// auto currentSample = (float) (std::sin (currentAngle) * level * tailOff);
+			// interpolating
+			auto index0 = (unsigned int) currentIndex;
+			auto index1 = index0 + 1;
+
+			auto frac = currentIndex - (float) index0;
+
+			auto table = wavetable.getReadPointer (0);
+			auto value0 = table[index0];
+			auto value1 = table[index1];
+
+			auto currentSample = (float) (value0 + frac * (value1 - value0)) * level;
+			
+			// // using floor index
+			// auto index0 = (unsigned int) currentIndex;
+			// auto table = wavetable.getReadPointer (0);
+			// auto currentSample = (float) table[index0] * level;
+			
+			if (tailOff > 0.0)
+			  currentSample *= tailOff;
+
+			for (auto i = outputBuffer.getNumChannels(); --i >= 0;)
+			  outputBuffer.addSample (i, startSample, currentSample);
+
+			++startSample;
+
+			if ((currentIndex += tableDelta) > (float) tableSize)
+			  currentIndex -= (float) tableSize;
+
+			if (tailOff > 0.0)
+			  {
+				tailOff *= 0.99; // [8]
+
+				if (tailOff <= 0.005)
+				  {
+					clearCurrentNote(); // [9]
+
+					tableDelta = 0.0;
+					break;
+				  }
+			  }
+		  }
+	  }
+  }
+  
+private:
+  // double currentAngle = 0.0, angleDelta = 0.0, level = 0.0, tailOff = 0.0;
+  double level = 0.0, tailOff = 0.0;
+  const juce::AudioSampleBuffer& wavetable;
+  const int tableSize;
+  float currentIndex = 0.0f, tableDelta = 0.0f;
+};
+
 //==============================================================================
-/*
-    This component lives inside our window, and this is where you should put all
-    your controls and content.
-*/
-class MainComponent  : public juce::AudioAppComponent, public juce::Timer
+class SynthAudioSource   : public juce::AudioSource
 {
 public:
-    //==============================================================================
-    MainComponent();
-    ~MainComponent() override;
+  SynthAudioSource (juce::MidiKeyboardState& keyState)
+	: keyboardState (keyState)
+  {
+	createWavetable();
+	
+	for (auto i = 0; i < 4; ++i)                // [1]
+	  synth.addVoice (new SineWaveVoice (sineTable));
 
-    //==============================================================================
-    void prepareToPlay (int samplesPerBlockExpected, double sampleRate) override;
-    void getNextAudioBlock (const juce::AudioSourceChannelInfo& bufferToFill) override;
-    void releaseResources() override;
+	synth.addSound (new SineWaveSound());       // [2]
+  }
 
-    //==============================================================================
-    void paint (juce::Graphics& g) override;
-  void timerCallback() override;
-    void resized() override;
+  void setUsingSineWaveSound()
+  {
+	synth.clearSounds();
+  }
+  
+  void createWavetable()
+  {
+	sineTable.setSize (1, (int) tableSize + 1);
+	auto* samples = sineTable.getWritePointer (0);
+
+	auto angleDelta = juce::MathConstants<double>::twoPi / (double) (tableSize -1);
+	auto currentAngle = 0.0;
+
+	for (unsigned int i = 0; i < tableSize; ++i)
+	  {
+		auto sample = std::sin (currentAngle);
+		samples[i] = (float) sample;
+		currentAngle += angleDelta;
+	  }
+	samples[tableSize] = samples[0];
+  }
+
+  void setupLoop(double spl) {
+	samplePerLoop = spl;
+	for (int i = 0; i < 2; i++) {
+	  // scriptedMidiEvents.addEvent(juce::MidiMessage::noteOn(0, 53, 1.0f), i*samplePerLoop);
+	  // scriptedMidiEvents.addEvent(juce::MidiMessage::noteOff(0, 53), std::floor(samplePerLoop/4) + i*samplePerLoop);
+	  // scriptedMidiEvents.addEvent(juce::MidiMessage::noteOn(0, 53, 1.0f), std::floor(samplePerLoop*3/8) + i*samplePerLoop);
+	  // scriptedMidiEvents.addEvent(juce::MidiMessage::noteOff(0, 53), std::floor(samplePerLoop/2) + i*samplePerLoop - 512);
+	  // scriptedMidiEvents.addEvent(juce::MidiMessage::noteOn(0, 60, 1.0f), std::floor(samplePerLoop/2) + i*samplePerLoop);
+	  // scriptedMidiEvents.addEvent(juce::MidiMessage::noteOff(0, 60), std::floor(samplePerLoop*3/4) + i*samplePerLoop);
+	  // scriptedMidiEvents.addEvent(juce::MidiMessage::noteOn(0, 60, 1.0f), std::floor(samplePerLoop*7/8) + i*samplePerLoop);
+	  // scriptedMidiEvents.addEvent(juce::MidiMessage::noteOff(0, 60), std::floor(samplePerLoop) + i*samplePerLoop - 512);
+	  scriptedMidiEvents.addEvent(juce::MidiMessage::noteOn(0, 59, 1.0f), i * samplePerLoop);
+	  scriptedMidiEvents.addEvent(juce::MidiMessage::noteOn(0, 53, 1.0f), i * samplePerLoop + 1);
+	  scriptedMidiEvents.addEvent(juce::MidiMessage::noteOn(0, 43, 1.0f), i * samplePerLoop + 2);
+	  scriptedMidiEvents.addEvent(juce::MidiMessage::noteOff(0, 59), std::floor(samplePerLoop*5/24) + i * samplePerLoop - 1);
+	  scriptedMidiEvents.addEvent(juce::MidiMessage::noteOff(0, 53), std::floor(samplePerLoop*5/24) + i * samplePerLoop - 2);
+	  scriptedMidiEvents.addEvent(juce::MidiMessage::noteOff(0, 43), std::floor(samplePerLoop*5/24) + i * samplePerLoop - 3);
+	  scriptedMidiEvents.addEvent(juce::MidiMessage::noteOn(0, 31, 1.0f), std::floor(samplePerLoop*5/24) + i * samplePerLoop);
+	  scriptedMidiEvents.addEvent(juce::MidiMessage::noteOff(0, 31), std::floor(samplePerLoop*1/4) + i * samplePerLoop - 1);
+	  scriptedMidiEvents.addEvent(juce::MidiMessage::noteOn(0, 43, 1.0f), std::floor(samplePerLoop*1/2) + i * samplePerLoop);
+	  scriptedMidiEvents.addEvent(juce::MidiMessage::noteOff(0, 43), std::floor(samplePerLoop*17/24) + i * samplePerLoop - 1);
+	  scriptedMidiEvents.addEvent(juce::MidiMessage::noteOn(0, 31, 1.0f), std::floor(samplePerLoop*17/24) + i * samplePerLoop);
+	  scriptedMidiEvents.addEvent(juce::MidiMessage::noteOff(0, 31), std::floor(samplePerLoop*3/4) + i * samplePerLoop - 1);
+	  scriptedMidiEvents.addEvent(juce::MidiMessage::noteOn(0, 36, 1.0f), std::floor(samplePerLoop*20/24) + i * samplePerLoop);
+	  scriptedMidiEvents.addEvent(juce::MidiMessage::noteOff(0, 36), std::floor(samplePerLoop*7/8) + i * samplePerLoop - 1);
+	  scriptedMidiEvents.addEvent(juce::MidiMessage::noteOn(0, 37, 1.0f), std::floor(samplePerLoop*7/8) + i * samplePerLoop);
+	  scriptedMidiEvents.addEvent(juce::MidiMessage::noteOff(0, 37), std::floor(samplePerLoop*23/24) + i * samplePerLoop - 1);
+	  scriptedMidiEvents.addEvent(juce::MidiMessage::noteOn(0, 38, 1.0f), std::floor(samplePerLoop*23/24) + i * samplePerLoop);
+	  scriptedMidiEvents.addEvent(juce::MidiMessage::noteOff(0, 38), (i+1) * samplePerLoop - 1);
+	  scriptedMidiEvents.addEvent(juce::MidiMessage::noteOn(0, 54, 1.0f), std::floor(samplePerLoop*23/24) + i * samplePerLoop + 1);
+	  scriptedMidiEvents.addEvent(juce::MidiMessage::noteOff(0, 54), (i+1) * samplePerLoop - 2);
+	  scriptedMidiEvents.addEvent(juce::MidiMessage::noteOn(0, 58, 1.0f), std::floor(samplePerLoop*23/24) + i * samplePerLoop + 2);
+	  scriptedMidiEvents.addEvent(juce::MidiMessage::noteOff(0, 58), (i+1) * samplePerLoop - 3);
+	}
+  }
+
+  void prepareToPlay (int /*samplesPerBlockExpected*/, double sampleRate) override
+  {
+	synth.setCurrentPlaybackSampleRate (sampleRate); // [3]
+	midiCollector.reset (sampleRate);
+	setupLoop (sampleRate*5);	   
+  }
+
+  void releaseResources() override {}
+
+  void getNextAudioBlock (const juce::AudioSourceChannelInfo& bufferToFill) override
+  {
+	bufferToFill.clearActiveBufferRegion();
+
+	juce::MidiBuffer incomingMidi;
+	midiCollector.removeNextBlockOfMessages (incomingMidi, bufferToFill.numSamples);
+	keyboardState.processNextMidiBuffer (incomingMidi, bufferToFill.startSample,
+										 bufferToFill.numSamples, true);       // [4]
+	// Adding scripted midi events
+	incomingMidi.addEvents(scriptedMidiEvents, bufferToFill.numSamples*currentCycle % samplePerLoop, bufferToFill.numSamples, 0);
+	synth.renderNextBlock (*bufferToFill.buffer, incomingMidi,
+						   bufferToFill.startSample, bufferToFill.numSamples); // [5]
+	currentCycle++;
+	if (currentCycle>=samplePerLoop)
+	  currentCycle -= samplePerLoop;
+  }
+  juce::MidiMessageCollector* getMidiCollector()
+  {
+	return &midiCollector;
+  }
 
 private:
-    //==============================================================================
-    // Your private member variables go here...
+  
+  juce::AudioSampleBuffer sineTable;
+  const unsigned int tableSize = 1 << 7;
+  
+  juce::MidiKeyboardState& keyboardState;
+  juce::Synthesiser synth;
+  juce::MidiMessageCollector midiCollector;
+  int currentCycle = 0;
+  juce::MidiBuffer scriptedMidiEvents;
+  int samplePerLoop;
+};
 
 
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
+//==============================================================================
+/*
+  This component lives inside our window, and this is where you should put all
+  your controls and content.
+*/
+class MainComponent  : public juce::AudioAppComponent,
+					   public juce::ChangeListener,
+					   public juce::Timer
+{
+public:
+  //==============================================================================
+  MainComponent();
+  ~MainComponent() override;
+
+  //==============================================================================
+  void prepareToPlay (int samplesPerBlockExpected, double sampleRate) override;
+  void getNextAudioBlock (const juce::AudioSourceChannelInfo& bufferToFill) override;
+  void releaseResources() override;
+  void setMidiInput (int);
+
+  //==============================================================================
+  void paint (juce::Graphics& g) override;
+  void timerCallback() override;
+  void resized() override;
+
+private:
+  void changeListenerCallback (juce::ChangeBroadcaster*) override {}
+  //==============================================================================
+  // Your private member variables go here...
+  juce::MidiKeyboardState keyboardState;
+  SynthAudioSource synthAudioSource;
+  juce::MidiKeyboardComponent keyboardComponent;
+
+  juce::ComboBox midiInputList;
+  juce::Label midiInputListLabel;
+  int lastInputIndex = 0;
+  juce::AudioDeviceSelectorComponent audioSetupComp;
+
+  JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
 };
